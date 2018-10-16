009b2fa598cdf32382d4bba213f11bf22130674b
Ayushi Gupta <31928561+ayu-gupta@users.noreply.github.com>
2018-10-16 20:50:01 +0530

    Add files via upload

---
+++ secret.py
@@ -0,0 +1,17 @@
+def email():
+	sender_id="enter sender email id"
+	return sender_id
+
+
+def pwd():
+	sender_pwd="enter sender password"
+	return sender_pwd
+
+def mysqlpwd():
+	mysql_pwd="enter mysql connection password"
+	return mysql_pwd
+
+
+def mysqluser():
+	mysql_user="enter mysql connection user"
+	return mysql_user
